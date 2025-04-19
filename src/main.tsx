f62982a1a2017c203d704b8eb742f634c851e8b3
medse <med.seffah@gmail.com>
2025-04-19 12:22:29 -0400

    Remove Jest configuration and update TypeScript settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {HashRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes, BrowserRouter} from 'react-router-dom';
 import './index.css';
 import DashboardLayout from "./layouts/DashboardLayout";
 import MaintenanceLayout from "./layouts/MaintenanceLayout";
@@ -41,7 +41,7 @@ WebFont.load({
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <HashRouter>
+            <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<LoginPage/>}/>
                     <Route path="/register" element={<SignUp/>}/>
@@ -63,7 +63,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                         </Route>
                     </Route>
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         </QueryClientProvider>
     </React.StrictMode>,
 );
